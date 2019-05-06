d635cb545b698ba6698b7d33447623692cc8ec1f
Jose <jgonzalezmn@gmail.com>
2019-05-06 17:14:01 -0400

    final project

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// Your code goes here
 
 // user gets thank you message for signing up
 const signMeUpBtn = document.querySelector (".btn");
@@ -22,12 +21,71 @@ drag.addEventListener("drag", function(){
   document.body.style.background = "black";
 });
 
-
+// logo turns yellow when double clicked 
 const logoHeading = document.querySelector(".logo-heading");
 logoHeading.addEventListener("mousedown", function(){
     console.log("logo clicked")
    logoHeading.style.color = "#F0E68C";
 });
 
+//user  double clicks an image to dissapear
 
+const imgVanish = document.querySelector(".img-fluid");
+imgVanish.addEventListener("click", function(){
+   imgVanish.parentNode.removeChild(imgVanish);
+});
+
+
+// key down alert
+
+const keyDwn = document.querySelector(".main-navigation");
+
+keyDwn.addEventListener("keydown", function(event){
+  alert("scroll down");
+  console.log(`user is pressing ${event.code}`)
+});
+
+// user has been on the page for 2 minutes alert
+
+// var audio = new Audio('fire.wav');
+// window.onload = setTimeout(function(){
+//    new Audio('fire.wav').play()
+//    alert('You are the winner')
+// }, 5000);
+
+
+
+//  console log when user has scrolled down
+
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+
+window.addEventListener('scroll', function(e) {
+   last_known_scroll_position = window.scrollY;
+ 
+   if (!ticking) {
+     window.requestAnimationFrame(function() {
+       console.log('hi')
+       ticking = false;
+     });
+     
+     ticking = true;
+   }
+ });
+ 
+
+// notifies user goes offline
+
+window.addEventListener('offline', function(){
+   alert('offline')
+ });
+
+
+
+ // user almost signed up
+signMeUpBtn.addEventListener('mouseover', function(){
+ console.log('Hovered over sign me up')
+});
 
